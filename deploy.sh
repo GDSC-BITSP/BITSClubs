5142cff2d1c9f45a05440a2f5ba85d7780ecd3a4
Arya Kulkarni <81806908+AryaKulkarni@users.noreply.github.com>
2022-10-29 15:21:51 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:AryaKulkarni/BITSClubs.git master:gh-pages
+git push -f https://github.com/GDSC-BITSP/BITSClubs master:gh-pages
 cd -
